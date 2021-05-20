726d46a60da6bfa34212e3922f71977c88f352e2
Andrii Kyreikov <Andropolog@gmail.com>
2021-05-20 20:31:17 +0300

    finish

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+import '../node_modules/material-design-icons';
+import './js/fetchAndRender';
